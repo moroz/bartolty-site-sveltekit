f29f8e4c42317d9440b1369332b76c044026a527
Karol Moroz <karol@moroz.dev>
2023-06-09 17:08:32 -0600

    Set up layout page

---
+++ src/components/layout/Layout.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import { SITE_TITLE } from '../../config';
+
+	export let title: string | undefined = undefined;
+
+	const pageTitle = [title, SITE_TITLE].filter(Boolean).join(' | ');
+</script>
+
+<svelte:head><title>{pageTitle}</title></svelte:head>
+<div class="layout">
+	<slot />
+</div>
+
+<style lang="scss">
+	:global(body) {
+		padding: 0;
+		margin: 0;
+	}
+
+	.layout {
+		background: salmon;
+	}
+</style>
